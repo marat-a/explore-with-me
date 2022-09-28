56ef13eedf441b4ecc31bf9c2e70716870545d59
marat-a <detishki66@yandex.ru>
2022-09-28 20:17:37 +0500

    feature: add main structure

---
+++ stat-server/src/main/java/ru/practicum/statserver/StatServerApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.statserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatServerApplication.class, args);
+    }
+
+}
